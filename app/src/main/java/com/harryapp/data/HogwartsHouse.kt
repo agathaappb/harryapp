71ecdbbc0759b593c2e3ba6ef6028032dbc60427
agathaappb <agatha.alana@hotmail.com>
2024-06-27 08:18:18 -0300

    iniciando estrutura com dados

---
+++ app/src/main/java/com/harryapp/data/HogwartsHouse.kt
@@ -0,0 +1,9 @@
+package com.harryapp.data
+
+enum class HogwartsHouse {
+    GRYFFINDOR,
+    HUFFLEPUFF,
+    REVENCLAW,
+    SLYTHERIN,
+    DEFAULT
+}
---
+++ app/src/main/java/com/harryapp/data/HousesMapper.kt
@@ -0,0 +1,13 @@
+package com.harryapp.data
+
+import com.harryapp.R
+
+fun HouseMapper() {
+    val house = mapOf(
+        HogwartsHouse.GRYFFINDOR to 1,
+        HogwartsHouse.HUFFLEPUFF to 1,
+        HogwartsHouse.REVENCLAW to 1,
+        HogwartsHouse.SLYTHERIN to 1,
+        HogwartsHouse.DEFAULT to R.drawable.harry_potter_house
+    )
+}
