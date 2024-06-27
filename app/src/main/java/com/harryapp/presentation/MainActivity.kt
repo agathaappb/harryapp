4c613776bb0494e0af9b1092ae877ad168ebf5d3
agathaappb <agatha.alana@hotmail.com>
2024-06-27 06:36:12 -0300

    abertura do fragment - hat selector

--- app/src/main/java/com/harryapp/presentation/MainActivity.kt
+++ app/src/main/java/com/harryapp/presentation/MainActivity.kt
@@ -2,11 +2,17 @@ package com.harryapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import androidx.viewbinding.ViewBinding
 import com.harryapp.R
+import com.harryapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
